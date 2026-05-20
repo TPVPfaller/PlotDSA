61dc24932e5418e967c6d2c7ec8be7a4a0945621
TPVPfaller <thomaspv.pfaller@gmail.com>
2026-05-20 15:58:16 +0200

    remove notch filter

--- plotdsa/core/calculations.py
+++ plotdsa/core/calculations.py
@@ -9,12 +9,9 @@ class DSACalculator:
 
     def __init__(self, window_sec):
         self.window_sec = window_sec
+        self._precompute_filter()
 
-        self.notch_freq = 50.0  # line noise
-
-        self._precompute_filters()
-
-    def _precompute_filters(self):
+    def _precompute_filter(self):
         fs = config.SAMPLE_RATE_HZ
 
         self.bp_b = firwin(
@@ -24,23 +21,37 @@ class DSACalculator:
             pass_zero=False,
             window='hamming'
         )
-
-        self.notch_b = firwin(
-            numtaps=81,
-            cutoff=[48.5, 51.5],
-            fs=fs,
-            pass_zero=True,
-            window='hamming'
-        )
-
         self.bp_a = [1.0]
-        self.notch_a = [1.0]
 
     def _apply_filters(self, data):
-        # Apply notch first, then bandpass
-        filtered = lfilter(self.notch_b, self.notch_a, data)
-        filtered = lfilter(self.bp_b, self.bp_a, filtered)
+        filtered = lfilter(self.bp_b, self.bp_a, data)
         return filtered
+
+    def filter_window(self, eeg_values):
+        return self._apply_filters(np.asarray(eeg_values, dtype=np.float32))
+
+
+    def compute_psd_from_filtered(self, filtered, method='multitaper'):
+        if method == 'multitaper':
+            psd = self.multitaper_method(filtered)
+        else:
+            _, psd = welch(
+                filtered,
+                fs=config.SAMPLE_RATE_HZ,
+                nperseg=config.N_PER_SEGMENT,
+                noverlap=None, # If None overlap will be 50%
+                return_onesided=True
+            )
+
+        if psd is None:
+            return np.full(len(config.FREQ_BINS), np.nan, np.float32)
+
+        psd = psd[config.FREQ_MASK]
+
+        if not np.all(psd):
+            return np.full(len(psd), np.nan, np.float32)
+
+        return psd
 
     def multitaper_method(self, eeg_values):
         """
@@ -49,7 +60,10 @@ class DSACalculator:
         TW = 1
         K = 3  # number of tapers
         nw = config.N_PER_SEGMENT # number of samples per window (800 for 400hz)
-        N = len(eeg_values)//config.N_PER_SEGMENT # number of windows
+        N = len(eeg_values) // nw # number of windows
+
+        if N == 0:
+            return None
 
         eeg_values = eeg_values[:(N * nw)] # cut off extra samples
         eeg_values = np.reshape(eeg_values, (N, nw)) # reshape to (N, nw)
@@ -84,28 +98,8 @@ class DSACalculator:
         if len(eeg_values) < min_samples:
             return None, None
 
-        # Apply FIR filters
-        filtered = self._apply_filters(np.asarray(eeg_values, dtype=np.float32))
-
-        if method == 'multitaper':
-            psd = self.multitaper_method(filtered)
-            if psd is None:
-                return np.full(len(config.FREQ_BINS), np.nan, np.float32)
-        else:
-            _, psd = welch(
-                filtered,
-                fs=config.SAMPLE_RATE_HZ,
-                nperseg=config.N_PER_SEGMENT,
-                noverlap=None, # If None then overlap is 50%
-                return_onesided=True
-            )
-
-        psd = psd[config.FREQ_MASK]
-
-        if not np.all(psd):
-            return np.full(len(psd), np.nan, np.float32)
-
-        return psd
+        filtered = self.filter_window(eeg_values)
+        return self.compute_psd_from_filtered(filtered, method=method)
 
     def update_config(self, window_sec):
         self.window_sec = window_sec
